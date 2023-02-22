09fea06eb5181647b2fc0c38fc651b1d6d25a5eb
aristrisnawan <aristrisnawan35@gmail.com>
2023-02-22 22:50:53 +0700

    add form (add data)

--- lib/add.dart
+++ lib/add.dart
@@ -11,11 +11,54 @@ class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text("Ini App bar"),
       ),
-      body: Center(
-        child: Text("Page add data"),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                TextFormField(
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                    hintText: 'Enter Your Name',
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  obscureText: false,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nim',
+                    hintText: 'Enter Your Nim',
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text("Reset"), style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red
+                    ),),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text("Submit")),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
